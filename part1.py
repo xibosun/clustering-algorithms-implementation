987e81204219b29596f617c469ac43df964125c2
Xibo Sun <xibosun@outlook.com>
2020-11-18 17:56:06 +0800

    add part one

---
+++ part1.py
@@ -0,0 +1,22 @@
+from sklearn.cluster import KMeans
+import numpy as np
+import random
+
+def getRepresentitivesByHibridSelection(fea, pSize, cntTimes = 10):
+    N = np.shape(fea)[0]
+    bigPSize = cntTimes * pSize
+    if pSize > N:
+        pSize = N
+    if bigPSize > N:
+        bigPSize = N
+    
+    selected_fea = random.sample([x for x in fea], k=bigPSize)
+    random_fea = np.array(selected_fea)
+
+    kmeans = KMeans(n_clusters=pSize).fit(random_fea)
+    return kmeans.cluster_centers_
+
+
+B = np.array([[1, 2, 2, 3], [2, 5, 0, 0], [1, 8, 3, 8], [3, 1, 2, 2], [3, 1, 0, 1]])
+repres = getRepresentitivesByHibridSelection(B, 2)
+print(repres)
